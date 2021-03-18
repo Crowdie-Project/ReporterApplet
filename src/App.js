1f3bc9bfa0b42bc1b9dddad4320340c0a937b8bf
gkahraman16 <gkahraman16@ku.edu.tr>
2021-03-18 16:01:35 +0300

    report submission to supabase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 //IMPORTS
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-// import Report from './Components/Report';
+import Report from './Components/Report';
 
 //ENVIRONMENT
 import env from './config/env';
@@ -76,16 +76,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-        <View style={styles.reportWrapper}>
-         
-         <Text style={styles.header}>Reporter Applet</Text>
-          
-         <TextInput placeholder="Code" style={styles.input}></TextInput>
-         <TextInput placeholder="Lat" style={styles.input}></TextInput>
-         <TextInput placeholder="Lon" style={styles.input}></TextInput>
-         <TextInput placeholder="Reporter" style={styles.input}></TextInput>
-         <Button title="submit" style={styles.btn} color="#662EDD"></Button>
-       </View> 
+        
+       <Report/>
     </View>
     
   );
@@ -97,24 +89,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#D1D1D1',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  reportWrapper: {
-    flex:1,
-    padding: 50
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 20
-  },
-  input: {
-    marginBottom: 20,
-    fontSize: 18,
-    height: 30,
-    paddingHorizontal: 5,
-    borderWidth: 1,
-  },
-  btn: {
-    
   }
 });
--- src/Components/Report.js
+++ src/Components/Report.js
@@ -3,57 +3,98 @@
 //IMPORTS AND REQUIRES
 
 //REACT IMPORTS
-import React from 'react';
-import {View, Text,TextInput, StyleSheet} from 'react-native';
+import React, { useRef, useState} from 'react';
+import {View, Text,TextInput, Button, StyleSheet} from 'react-native';
 
 //SUPABASE IMPORTS
 import { createClient } from '@supabase/supabase-js'
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabaseUrl = 'https://kivuanxiochrllqnepvh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTEyNDUzNCwiZXhwIjoxOTMwNzAwNTM0fQ.ETthLWIqTxoJsihnOrAYgCeFL0kkMd5v9-sEFmJ_kNA'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 //////////////////
 //MAIN
 //////////////////
 
 const Report = () => {
+
+  const [reports, setReports] = useState([]);
+  const codeRef = useRef();
+  const latRef = useRef();
+  const lonRef = useRef();
+  const reporterRef = useRef();
+  const [errorText, setError] = useState("");
+
+
+  const fetchReports = async () => {
+    let { data: reports, error } = await supabase
+        .from("TestReports")
+        .select("*")
+        .order("id", { ascending: false });
+    if (error) console.log("error", error);
+    else setReports(reports);
+};
+
+  const addReport = async () => {
+    let codeText = codeRef.current.value;
+    let code = codeText.trim();
+    let latText = latRef.current.value;
+    let lat = latText.trim();
+    let lonText = lonRef.current.value;
+    let lon = lonText.trim();
+   // let reporterText = reporterRef.current.value;
+   // let reporter = reporterText.trim();
+   
+    const { data: report, error } = await supabase
+    .from('TestReports')
+    .insert([
+      { CODE: code, LAT: lat, LON: lon},
+    ])
+    if (error) setError(error.message);
+    else {
+        setReports([report, ...reports]);
+        setError(null);
+        codeRef.current.value = "";
+        latRef.current.value = "";
+        lonRef.current.value = "";
+    }
+  };
+  
+  
+
     return (
        
             <View style={styles.reportWrapper}>
                 
                 <Text style={styles.header}>Reporter Applet</Text>
                 
-                <TextInput placeholder="Code" style={styles.input}></TextInput>
-                <TextInput placeholder="Lat" style={styles.input}></TextInput>
-                <TextInput placeholder="Lon" style={styles.input}></TextInput>
-                <TextInput placeholder="Reporter" style={styles.input}></TextInput>
+                <TextInput ref={codeRef} placeholder="Code" style={styles.input}></TextInput>
+                <TextInput ref={latRef} placeholder="Lat" style={styles.input}></TextInput>
+                <TextInput ref={lonRef} placeholder="Lon" style={styles.input}></TextInput>
+                <TextInput  placeholder="Reporter" style={styles.input}></TextInput>
                 
-                <Button title="submit" style={styles.btn} color="#662EDD"></Button>
+                <Button title="submit" onPress={addReport} style={styles.btn} color="#662EDD"></Button>
             </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#D1D1D1',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    reportWrapper: {
-      flex:1,
-      padding: 50
-    },
-    header: {
-      fontSize: 30,
-      fontWeight: 'bold',
-      marginBottom: 20
-    },
-    input: {
-      marginBottom: 20,
-      fontSize: 18,
-      height: 30,
-      paddingHorizontal: 5,
-      borderWidth: 1,
-    },
+  reportWrapper: {
+    flex:1,
+    padding: 50
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  input: {
+    marginBottom: 20,
+    fontSize: 18,
+    height: 30,
+    paddingHorizontal: 5,
+    borderWidth: 1,
+  },
     btn: {
       
     }
